a3aa5fd98dbcc6a511b89767a8ccdbb836edaf6b
Fernando Victor Alonso Martín <fvictoram@yahoo.es>
2022-07-08 19:43:53 +0200

    Adding a new unit test in WireTests

--- GeomC/WireTests.cpp
+++ GeomC/WireTests.cpp
@@ -72,3 +72,22 @@ TEST(WireTests, TestConcatenatedCloseWire) {
   EXPECT_TRUE(theWire.IsConcatenated());
   EXPECT_TRUE(theWire.IsClose());
 }
+
+TEST(WireTests, TestNotConcatenatedNotCloseWire) {
+  Wire theWire;
+  Edge theEdge;
+  Point thePoint;
+  thePoint.SetCoordinates(3.0, 4.5, 1.1);
+  theEdge.SetFinalPoint(thePoint);
+  theWire.Add(theEdge);
+  Point thePoint3;
+  thePoint3.SetCoordinates(3.5, 4.5, 1.1);
+  Edge theEdge2;
+  theEdge2.SetInitialPoint(thePoint3);
+  Point thePoint2;
+  thePoint2.SetCoordinates(4.0, 4.5, 1.1);
+  theEdge2.SetFinalPoint(thePoint2);
+  theWire.Add(theEdge2);
+  EXPECT_FALSE(theWire.IsConcatenated());
+  EXPECT_FALSE(theWire.IsClose());
+}
